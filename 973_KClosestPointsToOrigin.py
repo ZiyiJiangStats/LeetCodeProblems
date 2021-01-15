aea7e77a327517ded3c5b3cce4e495c5be1d83d2
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-01-14 22:19:17 -0800

    Create 973_KClosestPointsToOrigin.py

---
+++ 973_KClosestPointsToOrigin.py
@@ -0,0 +1,8 @@
+
+#Sort 
+
+class Solution: 
+    def kClosest(self,points,k): 
+        #sort the list of points based on the euclidean distance to the origin 
+        points.sort(key = lambda p:p[0]**2 + p[1]**2)  
+        return points[:k] 
