e793e3633257e146dd5f171ee8e194626a029591
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-01-16 19:09:51 -0800

    Update 206_reverseLinkedList_recursive.py

--- 206_reverseLinkedList_recursive.py
+++ 206_reverseLinkedList_recursive.py
@@ -2,8 +2,11 @@
 #recursive approach
 class Solution(object):
     def reverseList(self, head, prev=None):
+        # if head is None, returns the previous item 
+        # stopping condition 
         if not head:
           return prev
   
         curr, head.next = head.next, prev
+        #recursively call the function itself instead of using a while loop 
         return self.reverseList(curr, head)
