64ca23e20ddd545a52ee9614e4420d249aa0b713
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-01-16 19:07:43 -0800

    Update 206_reverseLinkedList.py

--- 206_reverseLinkedList.py
+++ 206_reverseLinkedList.py
@@ -4,6 +4,10 @@ class Solution:
 def reverseList(self, head):
     prev = None
     while head:
+        #move the head pointer to the next item 
+        #curr is a temporary variable 
+        # previous becomes the next 
+        # current becomes the previous 
         curr = head
         head = head.next
         curr.next = prev
