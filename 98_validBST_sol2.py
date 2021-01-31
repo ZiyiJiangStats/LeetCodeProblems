40623f46295cf2564a0a5ec1303d45bb403334b6
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-01-30 19:34:18 -0800

    Update 98_validBST_sol2.py

--- 98_validBST_sol2.py
+++ 98_validBST_sol2.py
@@ -8,10 +8,10 @@ class Solution(object):
             return True
         return self.isValidNode(root,float('-inf'), float('inf'))
     
-    def isValidNode(self, root, l, r):
+    def isValidNode(self, root, minVal, maxVal):
         if not root:
             return True
-        return l<root.val<r and self.isValidNode(root.left, l, root.val) and self.isValidNode(root.right, root.val, r)
+        return minVal<root.val<maxVal and self.isValidNode(root.left, minVal, root.val) and self.isValidNode(root.right, root.val, maxVal)
     
     
     #-inf is l which is the smallest possible value 
