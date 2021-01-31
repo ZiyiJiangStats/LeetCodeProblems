5e9f9782c224c008f81931e8d5d06996b4f30a97
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-01-30 19:16:01 -0800

    Update 98_validBST_sol2.py

--- 98_validBST_sol2.py
+++ 98_validBST_sol2.py
@@ -12,3 +12,9 @@ class Solution(object):
         if not root:
             return True
         return l<root.val<r and self.isValidNode(root.left, l, root.val) and self.isValidNode(root.right, root.val, r)
+    
+    
+    #-inf is l which is the smallest possible value 
+    # inf is r which is the largest possible value  
+    
+    # we want to make sure that     -inf < root.val < inf ,    -inf < root.left.val < root.val  ,  root.val < root.right.val < inf 
