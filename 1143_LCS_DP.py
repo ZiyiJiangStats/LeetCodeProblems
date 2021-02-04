e4b0cc3011d8abacf46b0d4f16ca7293937c62ba
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-02-04 02:02:34 -0800

    Update 1143_LCS_DP.py

--- 1143_LCS_DP.py
+++ 1143_LCS_DP.py
@@ -18,3 +18,31 @@ class Solution:
                     memo[row][col] = max(memo[row][col-1],memo[row-1][col]) 
          
          return memo[m][n] 
+
+      # Time complexity O(mn),   space complexity: O(mn) 
+    
+    
+    #Reduced space complexity 
+    
+    
+class Solution: 
+    def longestCommonSubsequence(self,s1:str, s2:str) -> int: 
+        m = len(s1) 
+        n = len(s2) 
+        if m<n: 
+            return self.longestCommonSubsequence(s2,s1) 
+        
+        memo = [[0 for _ in range(n+1)] for _ in range(2)]
+        
+        for i in range(m): 
+            for j in range(n): 
+                if s1[i] == s2[j]: 
+                    memo[1-i%2][j+1] = 1 + memo[i%2][j] 
+                    
+                 else: 
+                    memo[1-i%2][j+1] = max(memo[1-i%2][j],memo[i%2][j+1])
+                    
+         return memo[m%2][n]
+                    
+     # time complexity O(mn),   space complexity O(min(m,n))            
+        
