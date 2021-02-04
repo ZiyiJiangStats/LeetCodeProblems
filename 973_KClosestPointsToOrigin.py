e60a5d0c07e11ed8f69ae9e398012b43a28dd635
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-02-04 01:34:19 -0800

    Update 973_KClosestPointsToOrigin.py

--- 973_KClosestPointsToOrigin.py
+++ 973_KClosestPointsToOrigin.py
@@ -6,3 +6,6 @@ class Solution:
         #sort the list of points based on the euclidean distance to the origin 
         points.sort(key = lambda p:p[0]**2 + p[1]**2)  
         return points[:k] 
+
+    
+#Time Complexity O(nlogn),   space complexity O(n)
