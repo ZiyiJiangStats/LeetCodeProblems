6b184ed7585ba3d55773714ac75507c53c9a78d4
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-02-05 18:55:57 -0800

    Update 1143_LCS_recursive.py

--- 1143_LCS_recursive.py
+++ 1143_LCS_recursive.py
@@ -5,12 +5,19 @@
    class Solution:
         def longestCommonSubsequence(self, s1: str, s2: str) -> int:
             return self.helper(s1, s2, 0, 0)
-            
+         
+         #We start by assigning i=0, j=0 
+         #then we increment i,j by 1 every time we call the helper function again
         def helper(self, s1, s2, i, j):
+            #if we reach the end of the string, there is no matching between the s1 and s2, return 0 
             if i == len(s1) or j == len(s2):
                 return 0
+            # if we have a match at current position, we increase the LCS value 
             if s1[i] == s2[j]:
                 return 1 + self.helper(s1, s2, i + 1, j + 1)
+               
+            # if this is not a match, we move to the next character of either s1 or s2. 
+            # We return the maximum of the two scenarios 
             else:
                 return max(self.helper(s1, s2, i+1, j), self.helper(s1, s2, i, j + 1))
 
