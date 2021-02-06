59cc351296178fa1a5dd6c77633c307c961525ea
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-02-05 18:41:48 -0800

    Update 1143_LCS_DP.py

--- 1143_LCS_DP.py
+++ 1143_LCS_DP.py
@@ -8,13 +8,19 @@ class Solution:
     def longestCommonSubsequence(self,s1:str,s2:str) -> int : 
         m = len(s1) 
         n = len(s2) 
+        
+        #memo is a m+1 by n+1 matrix 
         memo = [[0 for _ in range(n+1)] for _ in range(m+1)]
         
+        #run m times 
         for row in range(1,m+1): 
             for col in range(1,n+1): 
+                #if the characters in two strings match
+                # The LCS increments 1 
                 if s1[row-1] == s2[col-1]: 
                     memo[row][col]= 1+ memo[row-1][col-1] 
                 else: 
+                # if they don't match, move to the next character in either s1 or s2, whichever gives the larger memo value 
                     memo[row][col] = max(memo[row][col-1],memo[row-1][col]) 
          
          return memo[m][n] 
@@ -44,5 +50,5 @@ class Solution:
                     
          return memo[m%2][n]
                     
-     # time complexity O(mn),   space complexity O(min(m,n))            
+     # time complexity O(mn),   space complexity O(min(m,n)), we only need 2 columns for memo this time         
         
