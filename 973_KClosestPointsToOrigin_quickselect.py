f9b250e2610c2f0214d37a90dd6da1c248214db6
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-02-06 21:57:57 -0800

    Create 973_KClosestPointsToOrigin_quickselect.py

---
+++ 973_KClosestPointsToOrigin_quickselect.py
@@ -0,0 +1,24 @@
+class Solution:
+    def kClosest(self, points, K):
+        self.sort(points, 0, len(points)-1, K)
+        return points[:K]
+    
+    def sort(self, points, l, r, K):
+        if l < r:
+            p = self.partition(points, l, r)
+            if p == K:
+                return
+            elif p < K:
+                self.sort(points, p+1, r, K)
+            else:
+                self.sort(points, l, p-1, K)
+            
+    def partition(self, points, l, r):
+        pivot = points[r]
+        a = l
+        for i in range(l, r):
+            if (points[i][0]**2 + points[i][1]**2) <= (pivot[0]**2 + pivot[1]**2):
+                points[a], points[i] = points[i], points[a]
+                a += 1
+        points[a], points[r] = points[r], points[a]                
+        return a
