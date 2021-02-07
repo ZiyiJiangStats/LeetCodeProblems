711f3fa0f83f2392fce67609ff3ea623e97971cd
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-02-06 21:47:03 -0800

    Update 973_KClosestPointsToOrigin_MinHeap.py

--- 973_KClosestPointsToOrigin_MinHeap.py
+++ 973_KClosestPointsToOrigin_MinHeap.py
@@ -10,12 +10,16 @@ import heapq
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
         
+        #we want to maintain a heap of size K 
         heap = []
         
         for (x, y) in points:
             dist = -(x*x + y*y)
+            #if heap is full, we push the item onto the heap. We then pop and return the smallest item on the heap
             if len(heap) == K:
                 heapq.heappushpop(heap, (dist, x, y))
+                
+            # if not full, we keep push onto the heap 
             else:
                 heapq.heappush(heap, (dist, x, y))
         
