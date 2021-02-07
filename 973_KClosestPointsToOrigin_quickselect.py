dd4c5035407ca4c229630a62b5bb024fd5edd491
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-02-06 22:05:35 -0800

    Update 973_KClosestPointsToOrigin_quickselect.py

--- 973_KClosestPointsToOrigin_quickselect.py
+++ 973_KClosestPointsToOrigin_quickselect.py
@@ -1,8 +1,11 @@
 class Solution:
     def kClosest(self, points, K):
+        #we want to partially sort the list of points by the distance. We only want the K closest points 
         self.sort(points, 0, len(points)-1, K)
+        #return the closest k points 
         return points[:K]
     
+    #sort l to r points 
     def sort(self, points, l, r, K):
         if l < r:
             p = self.partition(points, l, r)
@@ -12,7 +15,8 @@ class Solution:
                 self.sort(points, p+1, r, K)
             else:
                 self.sort(points, l, p-1, K)
-            
+      
+    #return a pivot 
     def partition(self, points, l, r):
         pivot = points[r]
         a = l
