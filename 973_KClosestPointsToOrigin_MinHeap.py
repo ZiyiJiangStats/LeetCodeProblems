bbc369cd783369c29ef11d07585d15ec12379364
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-02-06 21:42:22 -0800

    Update and rename 973_KClosestPointsToOrigin_DividNConquer.py to 973_KClosestPointsToOrigin_MinHeap.py

---
+++ 973_KClosestPointsToOrigin_MinHeap.py
@@ -0,0 +1,24 @@
+"""
+We want to do it in a way faster than O(nlogn) 
+
+We can use a minHeap
+"""
+
+
+import heapq
+
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        
+        heap = []
+        
+        for (x, y) in points:
+            dist = -(x*x + y*y)
+            if len(heap) == K:
+                heapq.heappushpop(heap, (dist, x, y))
+            else:
+                heapq.heappush(heap, (dist, x, y))
+        
+        return [(x,y) for (dist,x, y) in heap]
+    
+    # Time complexity O(NlogK),  space complexity O(K)
