e1ea17736de00b2420597fd4ed195efd6e5390c4
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-02-06 21:32:57 -0800

    Update 973_KClosestPointsToOrigin.py

--- 973_KClosestPointsToOrigin.py
+++ 973_KClosestPointsToOrigin.py
@@ -9,3 +9,13 @@ class Solution:
 
     
 #Time Complexity O(nlogn),   space complexity O(n)
+
+"""
+
+Input is a list of lists 
+points = [[3,3],[5,-1],[-2,4]]
+
+We sort the points by the distance which is evaluated with a lambda function 
+
+We return the first k elements in the list 
+"""
