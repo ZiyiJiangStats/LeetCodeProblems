3b76cf8cae4ea961e202a9a18c98290f62530e81
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-02-06 21:52:15 -0800

    Update 973_KClosestPointsToOrigin_MinHeap.py

--- 973_KClosestPointsToOrigin_MinHeap.py
+++ 973_KClosestPointsToOrigin_MinHeap.py
@@ -1,7 +1,7 @@
 """
 We want to do it in a way faster than O(nlogn) 
 
-We can use a minHeap
+We can use a maxHeap
 """
 
 
@@ -12,8 +12,10 @@ class Solution:
         
         #we want to maintain a heap of size K 
         heap = []
-        
+        #heap is by default minheap in python 
         for (x, y) in points:
+            #set it to negative. So the furthest points are stored on top of the heap
+            #we pop all the furthest points and only keep the closest K points in the heap. Then flatten the heap to a list 
             dist = -(x*x + y*y)
             #if heap is full, we push the item onto the heap. We then pop and return the smallest item on the heap
             if len(heap) == K:
