9f9ff9b075a33aa2071275be4d8e1c69dbed9f13
ZiyiJiangStats <47211339+ZiyiJiangStats@users.noreply.github.com>
2021-03-07 14:59:03 -0800

    Update 62_uniquePath.py

--- 62_uniquePath.py
+++ 62_uniquePath.py
@@ -10,3 +10,15 @@ class Solution:
                 else:
                     dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[m-1][n-1]
+
+    
+    
+    
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [1] * n
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[j] = dp[j - 1] + dp[j]
+        return dp[-1] if m and n else 0
